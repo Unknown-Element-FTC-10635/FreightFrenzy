6cc8d953988a09460dc8727d732668355af88c0d
Sean <sean2270@icloud.com>
2021-09-20 16:05:41 -0500

    Initial Commit Basic teleop class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/UKTeleOp.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TeleOp")
+public class UKTeleOp extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
